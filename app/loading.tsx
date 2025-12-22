d4108cb8f8642cbfaad76c378eb2e38873f7af76
Jeremy Cayabyab <cayabyabjeremy25@gmail.com>
2025-12-22 11:35:48 +0800

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-[60vh] space-y-4">
+        <div className="flex flex-col items-center justify-center min-h-[60vh] space-y-4 mb-36">
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
         </div>
     );
